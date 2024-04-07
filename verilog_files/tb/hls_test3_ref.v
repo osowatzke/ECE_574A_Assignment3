fc942bfca2841ae29a8b3e4f501f5b8b43e91e6b
Kazi Safkat <safkat@arizona.edu>
2024-04-07 13:52:24 -0700

    Update hls_test3_ref.v

--- verilog_files/tb/hls_test3_ref.v
+++ verilog_files/tb/hls_test3_ref.v
@@ -1,6 +1,6 @@
 `timescale 1ns/1ns
 
-module HLSM(Clk, Rst, Start, Done, a, b, c, d, e, f, g, h, num, avg);
+module HLSM_ref(Clk, Rst, Start, Done, a, b, c, d, e, f, g, h, num, avg);
 
     parameter LATENCY = 8;
     
