ee5a6131ded72765bfa7a3e559bc8f6ed3a808ec
Owen <osowatzke@gmail.com>
2024-04-13 20:00:32 -0700

    Update validation scripts

--- scripts/run_simulation.tcl
+++ scripts/run_simulation.tcl
@@ -1,5 +1,6 @@
 proc run_simulation {latency} {
     catch close_sim
+    set_property top HLSM_tb [get_fileset sim_1]
     set_property -name {xsim.simulate.runtime} -value {10000ns} -objects [get_filesets sim_1]
     set_property -name {xsim.elaborate.xelab.more_options} -value "-generic_top \"LATENCY=$latency\"" -objects [get_filesets sim_1]
     if {[catch launch_simulation]} {
